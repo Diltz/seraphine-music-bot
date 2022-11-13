0d4c98e59d5913f24a744ff23effa169fa2a95b6
Diltz <takpeshev.nicolai@gmail.com>
2022-11-14 05:18:58 +0700

    added mysql database setup

---
+++ util/setupDatabase.js
@@ -0,0 +1,17 @@
+// packages
+
+const mariadb = require("mariadb")
+
+//
+
+module.exports = async () => {
+    const connection = await mariadb.createConnection({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        database: process.env.MYSQL_DATABASE,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD
+    })
+
+    await connection.query("CREATE TABLE IF NOT EXISTS `servers` (guildId TEXT, isPremium BOOLEAN, language VARCHAR(2))")
+}
